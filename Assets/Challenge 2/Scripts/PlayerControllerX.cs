7ebf1020b679376a6c9b7c152ac215fe6452bf25
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-07-19 18:24:46 +0200

    Commit 1

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    public float startDelay = 1.0f;
+    public float spawnInterval = 4.0f;
+    private bool canCreateDogs;
+
+    private void Awake()
+    {
+        canCreateDogs = false;
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("AllowCreateDogs", startDelay, spawnInterval);
+    }
+
+    private void AllowCreateDogs()
+    {
+        canCreateDogs = !canCreateDogs;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && canCreateDogs)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            AllowCreateDogs();
+        }
+    }
+}
